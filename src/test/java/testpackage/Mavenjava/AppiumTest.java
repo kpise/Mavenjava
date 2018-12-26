57a4e128211f379555ec16145289b1c032e12d18
Kunal Pise <kpdevops12345@gmail.com>
2018-12-26 19:25:36 +0530

    initial commit

---
+++ src/test/java/testpackage/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package testpackage.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid(){
+		System.out.println("NativeAppAndroid");
+	}
+
+	
+	@Test
+	public void iOSApp(){
+		System.out.println("iOSApp");
+	}
+	
+}
